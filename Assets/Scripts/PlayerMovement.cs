a8b18c3ff39404b1cba4003208ebb1a07b799422
JGN5496 <johanngeorg.nitsche@yahoo.com>
2022-10-17 14:04:12 +0200

    Room Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float speed;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        var JoyStickAxis = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
+        float fixedY = player.position.y;
+
+        player.position += (transform.right * JoyStickAxis.x + transform.forward * JoyStickAxis.y) * Time.deltaTime * speed;
+        player.position = new Vector3(player.position.x, fixedY ,player.position.z);
+    }
+}
