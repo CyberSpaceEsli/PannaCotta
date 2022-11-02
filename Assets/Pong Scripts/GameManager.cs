0adb90b95b08dd7f7ad34854ebc403f2a0b7e266
Tillman Koch <tillmankoch@dreebit.com>
2022-11-02 12:47:25 +0100

    Pong add reset

---
+++ Assets/Pong Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public IntelligentBall ball;
+
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+
+    public Text playerScoreText;
+
+    public Text computerScoreText;
+
+    private int playerScore;
+
+    private int computerScore;
+
+    private void Start()
+        {
+            NewGame();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                StartRound();
+            }
+            if (Input.GetKeyDown(KeyCode.N)) {
+                NewGame();
+            }
+        }
+
+        public void NewGame()
+        {
+            SetPlayerScore(0);
+            SetComputerScore(0);
+            StartRound();
+        }
+
+        public void StartRound()
+        {
+            playerPaddle.ResetPosition();
+            computerPaddle.ResetPosition();
+            ball.ResetPosition();
+            ball.AddStartingForce();
+        }
+
+        public void PlayerScores()
+        {
+            SetPlayerScore(playerScore + 1);
+            StartRound();
+        }
+
+        public void ComputerScores()
+        {
+            SetComputerScore(computerScore + 1);
+            StartRound();
+        }
+
+        private void SetPlayerScore(int score)
+        {
+            playerScore = score;
+            playerScoreText.text = score.ToString();
+        }
+
+        private void SetComputerScore(int score)
+        {
+            computerScore = score;
+            computerScoreText.text = score.ToString();
+        }
+
+}
